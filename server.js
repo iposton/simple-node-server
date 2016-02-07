e392cf55a36a560c4aba3d5adeed342f0bc2129f
iposton <ianposton@sbcglobal.net>
2016-02-07 13:35:15 -0800

    add route request from server respond with json

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 app = express();
 
+app.get('/', function(req, res) {
+	var name = 'Ian';
+	res.json('My name is ' + name);
+});
+
 app.listen(3000, function(err) {
 	if(err) throw err;
 	console.log('Server running on port 3000');
