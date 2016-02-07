afc7c98e14ac4497ca912e6b6de58aebd2a5a131
iposton <ianposton@sbcglobal.net>
2016-02-07 14:07:41 -0800

    create html view with ejs template engine

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var ejs = require('ejs');
+var ejsMate = require('ejs-mate');
 
 app = express();
 
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+
+
+
+// app.get('/', function(req, res) {
+// 	var name = 'Ian';
+// 	res.json('My name is ' + name);
+// });
+
 app.get('/', function(req, res) {
-	var name = 'Ian';
-	res.json('My name is ' + name);
+	res.render('home');
 });
 
 app.listen(3000, function(err) {
